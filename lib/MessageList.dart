b5ed53df6598f758c8eb53ca5805abfa7d74913e
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2021-04-04 14:19:22 -0600

    Side Menu with Drawer Widget

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_email_client_app/ComposeButton.dart';
 import 'package:flutter_email_client_app/MessageDetail.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'Message.dart';
 
 
@@ -78,6 +79,68 @@ class _MessageListState extends State<MessageList> {
           }
         },
 
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text("myemail@email.com"),
+                accountEmail: Text("Sultan"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1602471615287-d733c59b79c4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"),
+              ),
+              otherAccountsPictures: [
+                GestureDetector(
+                  onTap: (){
+                    showDialog(context: context,
+                        builder: (context){
+                          return AlertDialog(
+                            title: Text("Adding new account..."),
+                      );
+                    });
+                  },
+                  child: CircleAvatar(
+                    child: Icon(Icons.add),
+                  ),
+                )
+              ],
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.inbox),
+              title: Text("Inbox"),
+              trailing: Chip(
+                label: Text("9", style: TextStyle(fontWeight: FontWeight.bold),),
+                backgroundColor: Colors.blue[100],
+              ),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.edit),
+              title: Text("Draft"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.archive),
+              title: Text("Archive"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.paperPlane),
+              title: Text("Sent"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.trash),
+              title: Text("Trash"),
+            ),
+            Divider(),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ListTile(
+                  leading: Icon(FontAwesomeIcons.cog),
+                  title: Text("Settings"),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
       floatingActionButton: ComposeButton(messages)
     );
