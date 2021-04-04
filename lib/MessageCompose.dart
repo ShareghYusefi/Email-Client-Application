9f722f42501c104f094e73f1ebf6382d237e8d17
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2021-04-04 06:54:09 -0600

    TextFormField & Validation Rules

--- lib/MessageCompose.dart
+++ lib/MessageCompose.dart
@@ -11,6 +11,8 @@ class _MessageComposeState extends State<MessageCompose> {
   String subject = "";
   String body = "";
 
+  final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,56 +20,60 @@ class _MessageComposeState extends State<MessageCompose> {
         title: Text('Compose Message'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                title: TextField(
-                  onChanged: (value){
-                    to = value;
-                  },
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.add),
-                      labelText: 'TO',
+        child: Form(
+          key: key,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: TextFormField(
+                    // if value does not contain @ sign, throw error message
+                    validator: (value) => !value!.contains('@') ? "'To' Field must be a valid email" : null,
+                    onSaved: (value) => to = value!,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.add),
+                        labelText: 'TO',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: TextFormField(
+                    validator: (value) => value!.length == 0 ? "'Subject' Field is required" : null,
+                    onSaved: (value) => subject = value!,
+                    decoration: InputDecoration(
+                        labelText: 'SUBJECT',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                    ),
+                  ),
+                ),
+                Divider(),
+                ListTile(
+                  title: TextFormField(
+                    onSaved: (value) => body = value!,
+                    decoration: InputDecoration(
+                      labelText: 'BODY',
                       labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                    ),
+                    maxLines: 8,
                   ),
                 ),
-              ),
-              ListTile(
-                title: TextField(
-                  onChanged: (value){
-                    subject = value;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'SUBJECT',
-                      labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                ListTile(
+                  title: ElevatedButton(
+                    child: Text('SEND'),
+                    onPressed: () {
+                      if (this.key.currentState!.validate()) {
+                        // Use the key reference to save/assign the state
+                        this.key.currentState!.save();
+                        Message message = Message(subject, body);
+                        Navigator.pop(context, message);
+                      }
+                    },
                   ),
-                ),
-              ),
-              Divider(),
-              ListTile(
-                title: TextField(
-                  onChanged: (value){
-                    body = value;
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'BODY',
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold)
-                  ),
-                  maxLines: 8,
-                ),
-              ),
-              ListTile(
-                title: ElevatedButton(
-                  child: Text('SEND'),
-                  onPressed: () {
-                    Message message = Message(subject, body);
-                    Navigator.pop(context, message);
-                  },
-                ),
-              )
-            ],
-          ),
+                )
+              ],
+            ),
+        ),
       ),
     );
   }
